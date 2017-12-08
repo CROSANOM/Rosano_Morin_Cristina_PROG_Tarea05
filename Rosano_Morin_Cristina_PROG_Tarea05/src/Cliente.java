96c6e621724a0abfd35ab76de846dcc143b1ae9e
crosanom <crosanom@192.168.1.43>
2017-12-08 07:05:00 +0100

    Se corrige el metodo constructor, eliminando el identificador como parametro

--- Rosano_Morin_Cristina_PROG_Tarea05/src/Cliente.java
+++ Rosano_Morin_Cristina_PROG_Tarea05/src/Cliente.java
@@ -8,4 +8,51 @@
  */
 public class Cliente {
 
+	private String nombre, direccion, localidad, codigoPostal;
+	int identificador, numClientes;
+
+	// crear el constructor clase Cliente con parametros
+	/**
+	 * @param nombre
+	 * @param direccion
+	 * @param localidad
+	 * @param codigoPostal
+	 * @param identificador
+	 * @param numClientes
+	 */
+	public Cliente(String nombre, String direccion, String localidad, String codigoPostal, int identificador,
+			int numClientes) {
+
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public int getIdentificador() {
+		return identificador;
+	}
+
+	public int getNumClientes() {
+		return numClientes;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nombre=" + nombre + ", direccion=" + direccion + ", localidad=" + localidad + ", codigoPostal="
+				+ codigoPostal + ", identificador=" + identificador + ", numClientes=" + numClientes + "]";
+	}
+
 }
