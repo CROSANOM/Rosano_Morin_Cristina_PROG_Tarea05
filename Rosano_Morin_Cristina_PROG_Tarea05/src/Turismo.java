3715d68de8b7e311a8c7bd959bff6bb725e4200d
crosanom <crosanom@192.168.1.43>
2017-12-08 07:07:36 +0100

    Tarea 5 Por ahora se deja solo con los atributos sin lanzar excepción + los metodos get + ToString

--- Rosano_Morin_Cristina_PROG_Tarea05/src/Turismo.java
+++ Rosano_Morin_Cristina_PROG_Tarea05/src/Turismo.java
@@ -6,6 +6,31 @@
  * @author crosanom
  *
  */
-public class Turismo {
 
+public class Turismo {
+	
+	private String matricula,marca,modelo;
+	private int cilindrada;
+
+	
+	/**
+	 * @param matricula
+	 * @param marca
+	 * @param modelo
+	 * @param cilindrada
+	 */
+	public Turismo(String matricula,String marca,String modelo,int cilindrada) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
+
