62f683f1cd2181622eb58d637fce18339ffb8211
crosanom <crosanom@192.168.1.43>
2017-12-08 07:31:01 +0100

    Crea la clase AlquilerVehiculos con sus atributos y constructor por defecto. Por ahora se deja en este punto

--- Rosano_Morin_Cristina_PROG_Tarea05/src/AlquilerVehiculos.java
+++ Rosano_Morin_Cristina_PROG_Tarea05/src/AlquilerVehiculos.java
@@ -1,4 +1,27 @@
 
+/**
+ * @author crosanom
+ *
+ */
 public class AlquilerVehiculos {
+	private int MAX_TURISMO, MAX_CLIENTES, MAX_ALQUILERES;
+
+	// constructor por defecto
+	
+	public AlquilerVehiculos() {
+
+	}
+
+	public int getMAX_TURISMO() {
+		return MAX_TURISMO;
+	}
+
+	public int getMAX_CLIENTES() {
+		return MAX_CLIENTES;
+	}
+
+	public int getMAX_ALQUILERES() {
+		return MAX_ALQUILERES;
+	}
 
 }
