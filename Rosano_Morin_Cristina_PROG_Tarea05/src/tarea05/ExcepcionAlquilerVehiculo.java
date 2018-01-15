9a59f628433cb9ea3ea591181524f97ae9f7a721
crosanom <crosanom@192.168.1.36>
2018-01-15 19:11:45 +0100

    Punto4 clase Alquiler_Vehiculos ( constructor msm como parametro)

---
+++ Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/ExcepcionAlquilerVehiculo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tarea05;
+
+/**
+ * @author crosanom
+ *
+ */
+
+public class ExcepcionAlquilerVehiculo {
+
+	String mensaje = "";
+
+	/* constructor */
+	public ExcepcionAlquilerVehiculo(String mensaje) {
+
+	}
+
+}
