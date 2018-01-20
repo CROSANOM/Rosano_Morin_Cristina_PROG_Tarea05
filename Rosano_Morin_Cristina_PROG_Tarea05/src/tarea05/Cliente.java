996e9ea6415f34bef1ff3e1db6c37a6971e485ce
crosanom <crosanom@192.168.1.36>
2018-01-20 11:18:51 +0100

    Commit Punto 5 : CREACIÓN DE CLASE con sus atributos. -Se corrige la declaración de los atributos en cuanto al modo de acceso.Inlcuye static en para el atributo identificador. - Se limpia el codigo eliminado el trozo de código que pertence al siguientes puntos ( constructor de la clase  y metodos get )

--- Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/Cliente.java
+++ Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/Cliente.java
@@ -1,62 +1,15 @@
 package tarea05;
-/**
- * 
- */
 
-/**
- * @author crosanom
- *
- */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// crear la clase cliente con los atributos y visibilidad adecuados 
 
 public class Cliente {
 
 	private String nombre, dni, direccion, localidad, codigoPostal;
-	private int identificador, numClientes;
+	private int identificador;
+	private static int numClientes;
 
-	// crear el constructor clase Cliente con parametros
-
-	/**
-	 * @param nombre
-	 * @param dni
-	 * @param direccion
-	 * @param localidad
-	 * @param codigoPostal
-	 * @param identificador
-	 * @param numClientes
-	 */
-	public Cliente(String nombre, String dni, String direccion, String localidad, String codigoPostal,
-			int identificador, int numClientes) {
-
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public String getDireccion() {
-		return direccion;
-	}
-
-	public String getLocalidad() {
-		return localidad;
-	}
-
-	public String getCodigoPostal() {
-		return codigoPostal;
-	}
-
-	public int getIdentificador() {
-		return identificador;
-	}
-
-	public int getNumClientes() {
-		return numClientes;
-	}
-
-	@Override
-	public String toString() {
-		return "Cliente [nombre=" + nombre + ", direccion=" + direccion + ", localidad=" + localidad + ", codigoPostal="
-				+ codigoPostal + ", identificador=" + identificador + ", numClientes=" + numClientes + "]";
-	}
-
+	
 }
