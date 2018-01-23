c7652754ff68a65c9c4091ce5e9b0062fdc2c532
crosanom <crosanom@192.168.1.36>
2018-01-23 19:31:27 +0100

    Punto 14 Se esboza la creación del constructor de la clase Alquiler, se volverá a este punto más tarde.

--- Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/Alquiler.java
+++ Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/Alquiler.java
@@ -1,73 +1,32 @@
 package tarea05;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-/**
- * 
- */
 
 /**
  * @author crosanom
  *
  */
 
-// punto 13 crear la clase Alquiler 
+// punto 13 crear la clase Alquiler con sus atributos
 
 public class Alquiler {
 
 	private Date fecha;
-	private int dias , MS_DIA;
-	private final SimpleDateFormat FORMATO_FECHA=new SimpleDateFormat("dd/MM/yyyy");// incialiazación del objeto 
-	private final double PRECIO_DIA=0;
-	
-	// punto 14 crear un constructor clase Alquiler con Cliente Turismo como parametros 
-	
-public Alquiler (Cliente cliente, Turismo turismo) {
-	// como inicializar los atributos de otra clase ;
-	
-	 cliente= cliente;
-	 turismo=turismo;
-	 //turismo.setDisponibilidad(false);
-	 
-	
-}
-	
-// punto 15  metodos get and set 
+	private int dias, MS_DIA;
+	private final SimpleDateFormat FORMATO_FECHA = new SimpleDateFormat("dd/MM/yyyy");// incialiazación del objeto
+	private final double PRECIO_DIA = 0;
 
-	public Date getFecha() {
-		return fecha;
+	// punto 14 crear un constructor clase Alquiler con Cliente Turismo como
+	// parametros
+
+	public Alquiler(Cliente cliente, Turismo turismo) {
+		// como inicializar los atributos de otra clase ;
+
+		cliente = cliente;
+		turismo = turismo;
+		// turismo.setDisponibilidad(false);
+
 	}
 
-	public int getDias() {
-		return dias;
-	}
-
-	public int getMS_DIA() {
-		return MS_DIA;
-	}
-
-	public SimpleDateFormat getFORMATO_FECHA() {
-		return getFORMATO_FECHA();
-	}
-
-	public double getPRECIO_DIA() {
-		return PRECIO_DIA;
-	}
-
-
-	// 
-	
-	
-	
-	// punto 18 
-	@Override
-	public String toString() {
-		return "Alquiler [fecha=" + fecha + ", dias=" + dias + ", MS_DIA=" + MS_DIA + ", FORMATO_FECHA=" + FORMATO_FECHA
-				+ ", PRECIO_DIA=" + PRECIO_DIA + "]";
-	}
-	
-	
-	
-	
-	
 }
