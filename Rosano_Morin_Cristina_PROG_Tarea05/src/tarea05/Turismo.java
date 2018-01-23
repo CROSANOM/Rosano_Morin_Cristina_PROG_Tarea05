8f426a963fd3b1c4dec5bea63ffaa72077582f90
crosanom <crosanom@192.168.1.36>
2018-01-23 18:15:41 +0100

    Prueba conectividad para commit punto 9

--- Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/Turismo.java
+++ Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/Turismo.java
@@ -3,62 +3,19 @@ package tarea05;
  * 
  */
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * @author crosanom
+ * @author crosanom version1
  *
  */
-
+// punto 9 crear la clase Turismo con sus atributos
 public class Turismo {
-	
-	private String matricula,marca,modelo;
+
+	private String matricula, marca, modelo;
 	private int cilindrada;
+	private boolean disponibilidad;
 
-	
-	/**
-	 * @param matricula
-	 * @param marca
-	 * @param modelo
-	 * @param cilindrada
-	 */
-	public Turismo(String matricula,String marca,String modelo,int cilindrada) {
-		
-	}
-
-
-	public String getMatricula() {
-		return matricula;
-	}
-
-
-	public String getMarca() {
-		return marca;
-	}
-
-
-	public String getModelo() {
-		return modelo;
-	}
-
-
-	public int getCilindrada() {
-		return cilindrada;
-	}
-
-
-	@Override
-	public String toString() {
-		return "Turismo [matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", cilindrada="
-				+ cilindrada + "]";
-	}
-	
-	
-	
-	
-	
-	
-	
 	
 }
-
-
-
