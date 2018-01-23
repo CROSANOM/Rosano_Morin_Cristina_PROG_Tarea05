fb0efc19f6b8f3b7bfe897dd28b502c444baca5d
crosanom <crosanom@192.168.1.36>
2018-01-23 18:57:19 +0100

    Punto 10 : Se esboza el constructor de Turismo
    Se crea un constructor que recibe cuatro parametros 3 String y 1 int
    Cilindrada
    
    Se declara un metodo privado que comprueba Matricula con expresion
    regular. Se importan el paquete regex.* para usar la clase Pattern y
    Matcher.
    
    Queda pendiente redefinir el constructor con el metodo
    CompruebaMatricula y la Excepción ( pendiente volveré en otro momento)

--- Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/Turismo.java
+++ Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/Turismo.java
@@ -1,9 +1,11 @@
 package tarea05;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 
  */
-
-
 
 /**
  * @author crosanom version1
@@ -16,5 +18,39 @@ public class Turismo {
 	private int cilindrada;
 	private boolean disponibilidad;
 
-	
+	// punto 10 crear un constructor clase Turismo con 4 parametros ( matricula,
+	// marca, modelo, cilindrada)
+	/**
+	 * @param matricula
+	 * @param marca
+	 * @param modelo
+	 * @param cilindrada
+	 */
+	public Turismo(String matricula, String marca, String modelo, int cilindrada) {
+
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.cilindrada = cilindrada;
+
+	}
+
+	// Crear el metodo comprueba matricula
+
+	/* metodo privado comprueba matricula */
+	private boolean compruebaMatricula(String matricula) {
+
+		Pattern matriculaPatron = Pattern.compile(" \"^([0-9]{4})([A-Z&&[^AEIOIU]]{3}$)\"");
+		Matcher emparejador = matriculaPatron.matcher((matricula));
+		if (emparejador.matches()) {
+			System.out.println("La matricula  es Correcto");
+
+		} else {
+			System.out.println("La matricula es incorrecta ");
+
+		}
+		return emparejador.matches();
+
+	}
+
 }
