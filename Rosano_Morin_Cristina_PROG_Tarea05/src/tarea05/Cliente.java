dadd9291570fa2989123ed572849c46295009b80
crosanom <crosanom@192.168.1.36>
2018-01-23 17:45:03 +0100

    PUNTO 7: GENERAR LOS METODOS GET DE LOS ATRIBUTOS Se generan con el asistente del IDE source  Generate Get and Setter, se eliminan los set. ( Más adelante se modificara el punto 6 Constructor pero por ahora hago un salto al 7)

--- Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/Cliente.java
+++ Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/Cliente.java
@@ -1,5 +1,7 @@
 package tarea05;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // crear la clase cliente con los atributos y visibilidad adecuados 
 
@@ -11,6 +13,44 @@ public class Cliente {
 
 	// punto 6 ( crear un constructor Cliente con cinco parámetros)
 	public Cliente(String nombre, String dni, String direccion, String localidad, String codigoPostal) {
+		this.nombre = nombre;
+		this.dni = dni;
+		this.direccion = direccion;
+		this.localidad = localidad;
+		this.codigoPostal = codigoPostal;
+
+	}
+
+	// crear metodos comprueba
+
+	/* metodo privado comprueba dni */
+	private boolean compruebaDni(String dni) {
+
+		Pattern dniPatron = Pattern.compile("[0-9A-Z][0-9]{7}[A-Z]");
+		Matcher emparejador = dniPatron.matcher((dni));
+		if (emparejador.matches()) {
+			System.out.println(" El dni es Correcto");
+
+		} else {
+			System.out.println("La matricula es incorrecta, lanzar excepcion");
+
+		}
+		return emparejador.matches();
+
+	}
+
+	/* metodo privado comprueba codigo postal */
+	private boolean compruebaCodigoPostal(String codigoPostal) {
+		/* expresionRegular */
+		Pattern cpPatron = Pattern.compile("0[1-9][0-9]{3}|[1-4][0-9]{4}|5[0-2][0-9]{3}\n" + "\n" + "$");
+		Matcher emparejador = cpPatron.matcher((codigoPostal));
+		if (emparejador.matches()) {
+			System.out.println(" El codigo Postal es Correcto");
+		} else {
+			System.out.println("El codigo Postal es Correcto");
+		}
+
+		return emparejador.matches();
 
 	}
 
