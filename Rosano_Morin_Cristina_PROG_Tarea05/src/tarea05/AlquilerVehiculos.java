68d9daf81e104da82a1075621f0682488b5bd42a
crosanom <crosanom@192.168.1.36>
2018-01-28 09:32:02 +0100

    Punto19:CREAR LA CLASE ALQUILER DE VEHICULO + ATRIBUTOS CON SU VISIBILIDAD ADECUADA.
    /**DECLARAN LOS ATRIBUTOS DE LA CLASE
    	 *
    	 * La clase Taller AlquilerVehiculo 6 atributos, de 2 grandes tipos de
    atributos: Arrays []  Clientes, Vehiculos, Trabajos ) y int ( indican el
    valor maximo de clientes,vehiculos y trabajos que se pueden guardar en
    la aplicación. Los atributos int denominados MAX_...son constantes
    porque el array no es dinámico, por tanto MAX_... indica el tamaño o
    numero de posiciones del array. Cuando se declaran estos atributos es
    deseable inicializarlo a un valor para el ejemplo se han tomado como
    valor :
    	private int MAX_TURISMO = 120; // Se quiere reservar siempre un numero
    mayor de turismo que de alquiler
    	private int MAX_CLIENTES = 100;
    	private int MAX_ALQUILERES = 100;
    	 */

--- Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/AlquilerVehiculos.java
+++ Rosano_Morin_Cristina_PROG_Tarea05/src/tarea05/AlquilerVehiculos.java
@@ -4,25 +4,81 @@ package tarea05;
  * @author crosanom
  *
  */
-public class AlquilerVehiculos {
-	private int MAX_TURISMO, MAX_CLIENTES, MAX_ALQUILERES;
 
-	// constructor por defecto
-	
+// Punto19 Crear la clase AlquilerVehiculos con sus atributos y visibilidad
+// adecuados
+
+public class AlquilerVehiculos {
+
+	private int MAX_TURISMO = 100; // indicar el tamaño del array
+	private int MAX_CLIENTES = 50;
+	private int MAX_ALQUILERES = 50;
+
+	private Turismo[] turismo; // tipo Turismo
+	private Cliente[] clientes; // tipo Cliente
+	private Alquiler[] alquileres; // tipo Alquiler
+
+	// Punto2 0 constructor por defecto crear los atributos instancia arrays
+
 	public AlquilerVehiculos() {
+
+		clientes = new Cliente[MAX_CLIENTES];
+		turismo = new Turismo[MAX_TURISMO];
+		alquileres = new Alquiler[MAX_ALQUILERES];
 
 	}
 
+	// Punto21 Crea los metodos get para cada atributo
+	/**
+	 * @return the mAX_TURISMO
+	 */
 	public int getMAX_TURISMO() {
 		return MAX_TURISMO;
 	}
 
+	/**
+	 * @return the mAX_CLIENTES
+	 */
 	public int getMAX_CLIENTES() {
 		return MAX_CLIENTES;
 	}
 
+	/**
+	 * @return the mAX_ALQUILERES
+	 */
 	public int getMAX_ALQUILERES() {
 		return MAX_ALQUILERES;
+	}
+
+	/**
+	 * @return the turismo
+	 */
+	public Turismo[] getTurismo() {
+		return turismo;
+	}
+
+	/**
+	 * @return the clientes
+	 */
+	public Cliente[] getClientes() {
+		return clientes;
+	}
+
+	/**
+	 * @return the alquileres
+	 */
+	public Alquiler[] getAlquileres() {
+		return alquileres;
+	}
+
+	// Punto22 Crear el metodo get Cliente (String DNI )
+
+	public String getCliente(String dni) {
+
+		
+
+		return dni;
+
 	}
 
 }
